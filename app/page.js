c236a92c03dfe69774747d17f806b27e7e609315
Antoine Lascou <antoine.lascou@epsi.fr>
2024-10-15 11:17:42 +0200

    dev + home page

---
+++ app/page.js
@@ -0,0 +1,65 @@
+import styles from "./NavBarle.css";
+import NavBar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Grid from "@mui/material/Grid2";
+import { Box, Button, Typography } from "@mui/material";
+import HomeButton from "../components/HomeButton/HomeButton";
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <NavBar />
+      <div style={{ display: 'flex', flexGrow: 1, flexDirection: "column", justifyContent: 'center', paddingInline: 50 }}>
+        <Grid container justifyContent={"center"} alignItems={"center"} sx={{ marginBlock: 'auto' }}>
+          <Grid size={9}>
+            <Grid container justifyContent={"center"} alignItems={"center"} spacing={4} sx={{ marginBlock: 'auto' }}>
+              <Grid size={{ sm: 12, md: 6 }}>
+                <Typography
+                  style={{
+                    fontSize: 32,
+                    fontWeight: 400,
+                    textAlign: "left",
+                  }}
+                >
+                  L’APPRENTISSAGE SIMPLIFIÉ SUR LES ENJEUX DES
+                  <Typography
+                    component={"span"}
+                    style={{
+                      fontSize: 32,
+                      fontWeight: 400,
+                      textAlign: "left",
+                      color: 'red'
+                    }}
+                  >
+                    {" "}RÉSEAUX SOCIAUX
+                  </Typography>
+                </Typography>
+                <Button style={{ padding: 10 }} sx={{ width: '100%', background: 'linear-gradient(90deg, #0081A7 0%, #F07167 100%)', borderRadius: 1 }}>
+                  <Typography color="white" fontWeight="900">
+                    JE COMMENCE
+                  </Typography>
+                </Button>
+              </Grid>
+              <Grid size={{ sm: 12, md: 6 }}>
+                <img src="/images/home.jpg" alt="Home Image" style={{ width: '100%' }} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent={"center"} alignItems={"center"} spacing={6} style={{ marginBlock: 50 }}>
+          <Grid size={{ xs: 11, sm: 10, md: 3 }}>
+            <HomeButton title="127H" subtitle="De formation disponibles" color="#0081A7" />
+          </Grid>
+          <Grid size={{ xs: 11, sm: 10, md: 3 }}>
+            <HomeButton title="230" subtitle="Établissements partenaires" color="#F07167" />
+          </Grid>
+          <Grid size={{ xs: 11, sm: 10, md: 3 }}>
+            <HomeButton title="15" subtitle="Parcours par thèmes" color="#0081A7" />
+          </Grid>
+        </Grid>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,226 @@
+'use client'
+
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid2';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Thèmes', 'FAQ', 'À propos'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
+  return (
+    <Container maxWidth="xl" style={{ backgroundColor: 'white', padding: 10 }}>
+      <Grid container flexDirection="row">
+        <Grid size={4}>
+          <Grid container flexDirection="row">
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              
+              ETHIQ
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid size={4}>
+          <Grid container flexDirection="row" justifyContent="center">
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ color: 'white', display: 'block' }}
+              >
+                <Typography variant="body2" color="black">
+                  {page}
+                </Typography>
+              </Button>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid size={4}>
+          <Grid container flexDirection="row" justifyContent="flex-end" spacing={2}>
+            <Button color='#F07167' sx={{ backgroundColor: "#F07167", borderRadius: 1 }}>
+              <Typography variant="body2" color="white" fontWeight="900">
+                connexion
+              </Typography>
+            </Button>
+            <Button color='#0081A7' sx={{ backgroundColor: "#0081A7", borderRadius: 1 }}>
+              <Typography variant="body2" color="white" fontWeight="900">
+                inscription
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container >
+  );
+
+  return (
+    <AppBar position="static" color='white'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* WEB */}
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGOT
+          </Typography>
+
+          {/* MOBILE */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          {/* WEB */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBar;
