35488de28293dd5a9fc224bc56b2c67a29b756fc
Antoine Lascou <antoine.lascou@epsi.fr>
2024-10-17 11:31:05 +0200

    fix build

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,12 +1,13 @@
 'use client';
-import React from 'react';
+
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
 import "./page.css";
 import ProfileFormation from "../../components/ProfileFormation/ProfileFormation";
 import SchoolIcon from '@mui/icons-material/School';
 import EditIcon from '@mui/icons-material/Edit';
 
-const Page = () => {
+const PageComponent = () => {
     const searchParams = useSearchParams()
     const id = searchParams.get('id')
 
@@ -14,12 +15,12 @@ const Page = () => {
 
     const user = {
         id: id,
-        name:"Pierre",
+        name: "Pierre",
         picture: "/images/default_user.jpg",
         formations: [
-            {name: "Cyberharcélement", progress: 0.5, link: "/formation/1"},
-            {name: "Vol d'identité", progress: 0.3, link: "/formation/2"},
-            {name: "Réseaux sociaux", progress: 1, link: "/formation/3"},
+            { name: "Cyberharcélement", progress: 0.5, link: "/formation/1" },
+            { name: "Vol d'identité", progress: 0.3, link: "/formation/2" },
+            { name: "Réseaux sociaux", progress: 1, link: "/formation/3" },
         ]
     }
 
@@ -28,7 +29,7 @@ const Page = () => {
             <div className={"profile__header"}>
                 <div className={"profile__background"}></div>
                 <div className={"profile__bottom"}>
-                    <div className={"profile__avatar"} style={{ background: `url(${user.picture})`, borderRadius: 500,backgroundSize: "contain", width:80, height:80}}></div>
+                    <div className={"profile__avatar"} style={{ background: `url(${user.picture})`, borderRadius: 500, backgroundSize: "contain", width: 80, height: 80 }}></div>
                     <div className={"profile__infos"}>
                         <div className={"profile__username"}>{user.name}</div>
                         <div className={"profile__level"}>Enseignement supérieur</div>
@@ -38,16 +39,16 @@ const Page = () => {
 
             <div className={"profile__stats"}>
                 <div className={"profile__stat"}>
-                    <SchoolIcon className={"profile__stat__value"}/>
+                    <SchoolIcon className={"profile__stat__value"} />
                     <div className={"profile__stat__title"}>EPSI</div>
                 </div>
                 <div className={"profile__stat"}>
                     <div className={"profile__stat__value"}>58%</div>
                     <div className={"profile__stat__title"}>Effectués</div>
                 </div>
-                <div className={"profile__stat"} style={{ opacity: 0.3}}>
-                    <EditIcon className={"profile__stat__value"}/>
-                    <div className={"profile__stat__title"}>Modifier<br/>les informations</div>
+                <div className={"profile__stat"} style={{ opacity: 0.3 }}>
+                    <EditIcon className={"profile__stat__value"} />
+                    <div className={"profile__stat__title"}>Modifier<br />les informations</div>
                 </div>
             </div>
 
@@ -55,7 +56,7 @@ const Page = () => {
                 <div className={"profile__progress__title"}>Formations en cours</div>
                 <div className={"profile__progress__formations"}>
                     {user.formations.map((item, index) => (
-                        <ProfileFormation item={item} index={index} key={index}/>
+                        <ProfileFormation item={item} index={index} key={index} />
                     ))}
                 </div>
             </div>
@@ -63,5 +64,13 @@ const Page = () => {
     );
 };
 
-export default Page;
+const ProfilePage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <PageComponent />
+        </Suspense>
+    );
+};
+
+export default ProfilePage;
 
